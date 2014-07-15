82d70c6a2ab9d8e794281e5658cacc6472dffa6d
Andrew McPherson <andrewmcp333@gmail.com>
2014-07-15 19:21:25 -0400

    Put together a menu to begin the game.

--- states/MenuGameState.js
+++ states/MenuGameState.js
@@ -2,6 +2,13 @@ function MenuGameState()
 {
 	this.initiate = function()
 	{
+		$("#menu.view").find("[href=play]").click(function(event)
+		{
+			event.preventDefault();
+			console.log("Hello World!");
+			game.load(new PlayGameState());
+		});
+		
 		$("#menu.view").show();
 	}
 	
@@ -12,6 +19,7 @@ function MenuGameState()
 			game.load(new PlayGameState());
 		}
 	}
+	
 	this.terminate = function()
 	{
 		$("#menu.view").hide();
