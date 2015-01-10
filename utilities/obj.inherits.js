e39e3b682e697b500c2142c434507a7b26131b25
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-10 14:45:32 -0500

    Nuked the repository to begin yet again. :D

--- utilities/obj.inherits.js
+++
@@ -1,6 +0,0 @@
-inherits = function(subclass, supclass)
-{
-	subclass.constructor = subclass;
-	subclass.prototype = Object.create(supclass.prototype);
-	subclass.prototype.supconstructor = supclass;
-}
