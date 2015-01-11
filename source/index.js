0810a81af148e31cba1f9eba6d10b8c68a9f8bcf
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-10 20:04:51 -0500

    Began developing the structure of the game.

---
+++ source/index.js
@@ -0,0 +1,5 @@
+window.React = require("react/addons")
+window.Reflux = require("reflux")
+
+var Game = require("<scripts>/Game")
+React.render(<Game/>, document.body)
---
+++ source/scripts/Game.js
@@ -0,0 +1,11 @@
+var Game = React.createClass({
+    render: function() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+})
+
+module.exports = Game
