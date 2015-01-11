d339f748a44a2a0bad26f0a9998f89e3decbbc19
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-10 20:58:12 -0500

    Exposed the ratio of the frame to the component.

--- source/scripts/GameFrame.js
+++ source/scripts/GameFrame.js
@@ -1,7 +1,12 @@
 var GameFrame = React.createClass({
+    getDefaultProps: function() {
+        return {
+            ratio: "16x9"
+        }
+    },
     render: function() {
         return (
-            <div id="game-frame" className="_16x9">
+            <div id="game-frame" className={"_" + this.props.ratio}>
                 {this.props.children}
             </div>
         )
