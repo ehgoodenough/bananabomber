137dd277a6523d4459aaa6faf08b584cfabcc2f8
Andrew McPherson <andrewmcp333@gmail.com>
2015-01-10 21:19:36 -0500

    Redefined the scale of each unit in the frame.

--- source/scripts/Game.js
+++ source/scripts/Game.js
@@ -4,7 +4,14 @@ var Game = React.createClass({
     render: function() {
         return (
             <GameFrame>
-                Hello World!
+                <div style={{
+                    backgroundColor: "red",
+                    width: "1em",
+                    height: "1em",
+                    position: "absolute",
+                    top: "8em",
+                    left: "15em"
+                }}>!!!!!</div>
             </GameFrame>
         )
     }
