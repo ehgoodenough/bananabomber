28648b0a604cc8628652806852d5f8a19586df4f
Andrew McPherson <andrewmcp333@gmail.com>
2015-11-13 20:22:47 -0800

    Updated the build process with a wrapper.

--- build.js
+++ build.js
@@ -1,14 +1,59 @@
-var BrowserSync = require("browser-sync")
+var Webpack = require("webpack")
 var TextWebpackPlugin = require("extract-text-webpack-plugin")
 var BumpWebpackPlugin = require("bump-webpack-plugin")
-var Webpack = require("webpack")
+var BrowserSync = require("browser-sync")
 
-var yargs = require("yargs")
+var fs = require("fs")
 var del = require("del")
+var gulp = require("gulp")
+var yargs = require("yargs")
+var jsesc = require("jsesc")
+var ansiup = require("ansi_up")
 
-var package = require("./package.json")
+var Webpacker = function(config) {
+    this.webpack = Webpack(config)
+    this.config = config
+}
 
-var webpack = Webpack({
+Webpacker.prototype.run = function(func) {
+    this.webpack.run(this._handle(func))
+}
+
+Webpacker.prototype.watch = function(func) {
+    this.webpack.watch({}, this._handle(func))
+}
+
+Webpacker.prototype._handle = function(func) {
+    return function(error, results) {
+        if(results.compilation.errors.length > 0) {
+            var errors = results.compilation.errors.map(function(error) {
+                return "<div>" + ansiup.ansi_to_html(error.toString()) + "</div>"
+            }).join()
+            errors = "<style>div{font-family: monospace;white-space:pre;}</style>" + errors
+            errors = "document.body.innerHTML = \"" + jsesc(errors, {quotes: "double"}) + "\""
+            fs.writeFile("./build/web/index.js", errors, function() {
+                func()
+            })
+        } else {
+            func()
+        }
+    }
+}
+
+var BrowserSyncer = function(config) {
+    this.browsersync = undefined
+    this.config = config
+}
+
+BrowserSyncer.prototype.reload = function() {
+    if(this.browsersync == undefined) {
+        this.browsersync = BrowserSync(this.config)
+    } else {
+        this.browsersync.reload()
+    }
+}
+
+var webpacker = new Webpacker({
     entry: {
         "index.js": "./source/index.js",
         "index.html": "./source/index.html",
@@ -31,30 +76,28 @@ var webpack = Webpack({
     ]
 })
 
+var browsersyncer = new BrowserSyncer({
+    server: "./build/web",
+    logLevel: "silent",
+    notify: false,
+    port: 1234,
+})
+
+var logger = function(string) {
+    console.log(string)
+}
+
 del("./build/web").then(function() {
     if(yargs.argv._.indexOf("server") != -1) {
-        var browsersync = BrowserSync({
-            server: "./build/web",
-            logLevel: "silent",
-            notify: false,
-            port: 1234,
-        })
-        webpack.watch({}, function(error, results) {
-            console.log("Building ``./source/index.js`` for [web].")
-            if(results.compilation.errors.length > 0) {
-                results.compilation.errors.forEach(function(error) {
-                    console.log(error.toString())
-                })
-            } else {
-                browsersync.reload()
-            }
+        webpacker.watch(function() {
+            logger("Watching ``./source/index.js``")
+            browsersyncer.reload()
         })
     } else {
-        webpack.run(function(error, results) {
-            console.log("Building from ``./source/**/*`` for [web].")
-            results.compilation.errors.forEach(function(error) {
-                console.log(error.toString())
-            })
+        webpacker.run(function() {
+            logger("Running ``./source/index.js``")
         })
     }
+    gulp.src("./source/index.css").pipe(gulp.dest("./build/web/"))
+    gulp.src("./source/index.html").pipe(gulp.dest("./build/web/"))
 })
